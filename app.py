c950607b31bab9b16a2fc3121ab5913029dec1ef
hannalakew <92497612+hannalakew@users.noreply.github.com>
2022-02-24 01:02:22 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 # import necessary libraries
 # from models import create_classes
-from distutils.log import debug
 import pandas as pd
 import os
 import sqlalchemy
@@ -16,21 +15,20 @@ from flask import (
     jsonify,
     request,
     redirect,
-    jsonify,
-    url_for) 
+    jsonify)
 
 # Read data from csv
-# csv_file = "data/Chicago Health Atlas.csv"
-# df = pd.read_csv(csv_file)
-# df.head()
-# df.rename(columns={"VRDIBR_2015-2019":"VRDIBR_2015_2019","VRDIAR_2015-2018":"VRDIAR_2015_2018","VRDTH_2015-2019":"VRDTH_2015_2019","VRCAR_2015-2019":"VRCAR_2015_2019","VRADR_2015-2019":"VRADR_2015_2019","HDX_2015-2019":"HDX_2015_2019"},inplace=True)
+#csv_file = "data/Chicago Health Atlas.csv"
+#df = pd.read_csv(csv_file)
+#df.head()
+#df.rename(columns={"VRDIBR_2015-2019":"VRDIBR_2015_2019","VRDIAR_2015-2018":"VRDIAR_2015_2018","VRDTH_2015-2019":"VRDTH_2015_2019","VRCAR_2015-2019":"VRCAR_2015_2019","VRADR_2015-2019":"VRADR_2015_2019","HDX_2015-2019":"HDX_2015_2019"},inplace=True)
 
-# creating sqlite engine to create database
-# engine = create_engine('sqlite:///data/Chicago_Health_database.db')
-# engine = create_engine('sqlite:///C:/Users/doyel/Desktop/project3_flask_ex1/data/mydatabase.db')
-# Table name : Chicago_Health_Atlas
+#creating sqlite engine to create database
+#engine = create_engine('sqlite:///data/Chicago_Health_database.db')
+#engine = create_engine('sqlite:///C:/Users/doyel/Desktop/project3_flask_ex1/data/mydatabase.db')
+#Table name : Chicago_Health_Atlas
 
-# df.to_sql('Chicago_Health_Atlas',con=engine,if_exists='replace')
+#df.to_sql('Chicago_Health_Atlas',con=engine,if_exists='replace')
 #####################################################################
 engine = create_engine("sqlite:///data/mydatabase.db")
 
@@ -65,15 +63,20 @@ def map():
 
     return render_template("map.html")
 
+@app.route("/plot")
+def plot():
 
+    return render_template("plot.html")
 # ---------------------------------------------------------
 
 # API to call "when data.html" page is loading with community information table
 @app.route("/api/community")
 def community_grid():
     session = Session(engine)
-  
-    results = session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population,Healthatlas.Longitude, Healthatlas.Latitude).all()
+    
+    
+    results = session.query(Healthatlas.Name,Healthatlas.Median_Household_Income,Healthatlas.Poverty_Rate,Healthatlas.Receiving_Food_Stamps,Healthatlas.Public_Assistance_Income,Healthatlas.High_School_Grad_Rate, Healthatlas.College_Grad_Rate,Healthatlas.Non_Hispanic_White,Healthatlas.Non_Hispanic_Black,Healthatlas.Asian_Pacific_Islander,Healthatlas.Hispanic_or_Latino,Healthatlas.Population_All,Healthatlas.Population_Infants,Healthatlas.Population_Juveniles,Healthatlas.Population_Young_Adults,Healthatlas.Population_Middle_Aged_Adults,Healthatlas.Population_Seniors).all()
+    #results = session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population,Healthatlas.Longitude, Healthatlas.Latitude).all()
     #results = pd.read_sql('SELECT Name,GEOID,Population,Longitude,Latitude FROM Chicago_Health_Atlas', engine)
     #results = engine.execute("SELECT Name,GEOID,Population,Longitude,Latitude FROM Chicago_Health_Atlas").fetchall()
     #session.query(Movies.title, Movies.director, Movies.year, Movies.rating, Movies.imdb_votes, Movies.imdb_score).all()
@@ -96,31 +99,43 @@ def deceases(decease):
     session = Session(engine)
 
     if decease == "diabetes":
-        results = session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRDIAR_2015_2019, Healthatlas.HDX_2015_2019).all()
-    if decease == "diabetes_related":
-        results = session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRDIBR_2015_2019, Healthatlas.HDX_2015_2019 ).all()
-    if decease == "alzheimer":
-        results = session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRADR_2015_2019, Healthatlas.HDX_2015_2019).all()
-    if decease == "cancer":
-        results == session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRCAR_2015_2019, Healthatlas.HDX_2015_2019).all()
-    if decease == "All" :
-        results = session.query(Healthatlas.Name,Healthatlas.GEOID, Healthatlas.Population, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRDTH_2015_2019, Healthatlas.HDX_2015_2019).all()
-
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRDIAR_2015_2019, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+    elif decease == "diabetes_related":
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRDIBR_2015_2019, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018 ).all()
+    elif decease == "alzheimer":
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRADR_2015_2019, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+    elif decease == "cancer":
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRCAR_2015_2019, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+    elif decease == "hypertension":
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.HCSHYTP_2016_2018, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+    elif decease == "adult_obesity":
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.HCSOBP_2016_2018, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+    elif decease == "coronary_heart_disease":
+        results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRCHDR_2015_2019, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+    #elif decease == "all" :
+     #   results = session.query(Healthatlas.Name, Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRDTH_2015_2019, Healthatlas.HDX_2015_2019).all()        
+    else:
+        results = session.query(Healthatlas.Name,Healthatlas.Population_All, Healthatlas.Longitude, Healthatlas.Latitude, Healthatlas.VRADR_2015_2019, Healthatlas.HDX_2015_2019, Healthatlas.HCSSP_2016_2018, Healthatlas.HCSSMKP_2016_2018).all()
+        
     
     
     results = [list(r) for r in results]
     
-    name = [result[5] for result in results]
+    name = [result[4] for result in results]
     
-    hardship = [result[6] for result in results]
+    hardship = [result[5] for result in results]
+    
+    soda = [result[6] for result in results]
+    
+    smoke = [result[7] for result in results]
     
     
     decease_results = {
         "decease_name": name,
         "hd_index": hardship,
+        "soda_con":soda,
+        "smoking":smoke,
     }
-
-    print(decease_results)
 
     session.close()
 
@@ -134,7 +149,5 @@ def map_data():
     return jsonify(your_data)
 
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
